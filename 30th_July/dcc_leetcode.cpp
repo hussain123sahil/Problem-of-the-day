4cfd8bc3bb1bdb7f3502d7ad09211db25e311f6e
sahil <sahilhussain1231@gmail.com>
2024-07-30 17:48:07 +0530

    updated codes

---
+++ 30th_July/dcc_leetcode.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int ans = 0;
+        int count = 0;
+        for (char x : s) {
+            if (x == 'b') {
+                count += 1;
+            } else if (count) {
+                ans += 1;
+                count -= 1;
+            }
+        }
+        return ans;
+    }
+};
