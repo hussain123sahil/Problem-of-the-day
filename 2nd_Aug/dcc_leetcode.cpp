ff3dca526ba6f96c8d8f28d208ea950ff1e2c85d
sahil <sahilhussain1231@gmail.com>
2024-08-02 22:25:56 +0530

    updated 2aug codes

---
+++ 2nd_Aug/dcc_leetcode.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int totalOnes= accumulate(nums.begin(),nums.end(),0);
+        int n= nums.size(),l=0,r=0,currOnes=0;
+        while(r<totalOnes){
+            if(nums[r]==1) currOnes++;
+            r++;
+        }
+        r=r%n;
+        int maxOnes=currOnes;
+        while(l<n){
+            if(nums[l]==1) currOnes--;
+            if(nums[r]==1) currOnes++;
+            maxOnes= max(maxOnes,currOnes);
+            r= (r+1)%n;
+            l++;
+        }
+        return totalOnes-maxOnes;
+    }
+};
