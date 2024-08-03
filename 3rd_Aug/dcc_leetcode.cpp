871bf715a099081c0be9d33870dd4dd36ed3f732
sahil <sahilhussain1231@gmail.com>
2024-08-03 16:55:31 +0530

    updated 3aug codes

---
+++ 3rd_Aug/dcc_leetcode.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            if(target[i]!=arr[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
