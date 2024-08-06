86e023e84a56b05f4beb33cc0a654fcfc89f8301
sahil <sahilhussain1231@gmail.com>
2024-08-06 15:00:24 +0530

    updated 6aug codes

---
+++ 6th_Aug/dcc_leetcode.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> hashMap(26,0);
+
+        for(auto &c: word){
+            hashMap[c-'a']++;
+        }
+
+        sort(hashMap.begin(),hashMap.end(),greater<int>());
+
+        int ans = 0;
+        int push = 1;
+        int keyCnt = 0;
+
+        for(auto &freq: hashMap){
+
+            if(freq == 0) break;
+
+            ans += freq * push;
+            keyCnt++;
+
+            if(keyCnt % 8 == 0) push++;
+            
+        }
+
+        return ans; 
+    }
+};
